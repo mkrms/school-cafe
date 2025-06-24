049d739bf48cb7023e2593074e4b2cc83a5a22de
mkrms <niiyama-k@unotame.com>
2025-06-25 00:15:49 +0900

    optimization minimize false

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,27 +4,34 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/kitchen/qr-reader',
+        source: "/kitchen/qr-reader",
         headers: [
           {
-            key: 'Permissions-Policy',
-            value: 'camera=*, microphone=()'
-          }
-        ]
-      }
-    ]
+            key: "Permissions-Policy",
+            value: "camera=*, microphone=()",
+          },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: "http",
+        hostname: "localhost",
       },
       {
         protocol: "https",
         hostname: "localhost:1338",
       },
     ],
+  },
+
+  webpack: (config) => {
+    // Disable minification
+    config.optimization.minimize = false;
+
+    return config;
   },
 };
 
